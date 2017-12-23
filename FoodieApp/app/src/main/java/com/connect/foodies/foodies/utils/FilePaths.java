d52032e7a4b5fa5231db7fb9f3fc77f32b91f238
rkadam <rkadam@syr.edu>
2017-12-23 09:30:51 -0500

    Foodie App Commit

---
+++ FoodieApp/app/src/main/java/com/connect/foodies/foodies/utils/FilePaths.java
@@ -0,0 +1,19 @@
+package com.connect.foodies.foodies.utils;
+
+import android.os.Environment;
+
+/**
+ * Created by User on 7/24/2017.
+ */
+
+public class FilePaths {
+
+    //"storage/emulated/0"
+    public String ROOT_DIR = Environment.getExternalStorageDirectory().getPath();
+
+    public String PICTURES = ROOT_DIR + "/Pictures";
+    public String CAMERA = ROOT_DIR + "/DCIM/Camera";
+    public String DOWNLOAD = ROOT_DIR + "/Download";
+    public String FIREBASE_IMAGE_STORAGE = "photos/users/";
+
+}
